9b207f9190558776cc49db79a181f192bf99f587
alixfanny <alix.fanny.mail@gmail.com>
2023-12-04 21:48:41 +0100

    modification du routage d'accées au profil

--- Frontend/bank/src/identification/routerProfil.js
+++ Frontend/bank/src/identification/routerProfil.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch} from 'react-redux';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import User from '../pages/user';
 import EditUserName from '../pages/editUserName';
 import { profileSuccess } from '../redux/reduceur/ProfilReduceur';
@@ -9,23 +9,36 @@ import { datas } from '../services/CallApi';
 const ProfilRouter = () => { 
 
     const dispatch = useDispatch();
-  
+    const navigate = useNavigate();
+    
+    const isAuthenticated = () => {
+        return localStorage.getItem('token') != null;
+    };
+
     useEffect(() => {
-     datas.username()
-      .then((response) => {
-      dispatch(profileSuccess(response.data.body));
-      })
-      .catch((error) => {
-      console.error('Erreur de connexion :', error);
-      });
-    }, [dispatch]); 
+        if (!isAuthenticated()) {
+            navigate('/sign-in');
+        } else {
+            datas.username()
+            .then((response) => {
+                dispatch(profileSuccess(response.data.body));
+            })
+            .catch((error) => {
+                console.error('Erreur de connexion :', error);
+            });
+        }
+    }, [dispatch, navigate]); 
 
     return (
         <Routes> 
-            <Route index element={<User />} />
-            <Route path="ModificationProfil" element={<EditUserName />} /> 
+            {isAuthenticated() && (
+                <>
+                    <Route index element={<User />} />
+                    <Route path="ModificationProfil" element={<EditUserName />} />
+                </>
+            )}
         </Routes>
     )
 }
 
-export default ProfilRouter
+export default ProfilRouter;
